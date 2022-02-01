047a9d9efd56da8bab3dd144c9d1aa0f0344c66a
ykkd <17854586+ykkd@users.noreply.github.com>
2022-02-02 02:24:38 +0900

    update

---
+++ playground.dart
@@ -0,0 +1,33 @@
+main() {
+  var a = "hello";
+  print(a);
+
+  String b = "world";
+
+  print('$a $b');
+
+  List array = [1, 2, 3];
+  print("$array");
+
+  // compile error
+  // Set set = [1, 1, 2, 3];
+
+  Set set = {1, 3, 4};
+  List convertedSet = set.toList();
+  print('$convertedSet');
+
+  dynamic j = 'this variable can be any types because of its dynamic type';
+  j = 5;
+  print(j);
+
+  // final can only be defined at initialization
+  final f = "final";
+
+  const c = "constant";
+
+  print('$f, $c');
+
+  // parse
+  print(int.parse('55'));
+  print(56.toString());
+}
